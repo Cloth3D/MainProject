17046d765f199c40bec0a2bb8b03da4f21f34996
江超群 <mcoder2014@sina.com>
2016-05-22 16:18:58 +0800

    work start

---
+++ WebContent/js/custom/load_model.js
@@ -0,0 +1,36 @@
+/**
+ * @author 江超群
+ * 专门用来读取我们自己的人体及衣物模型
+ */
+
+var LoadAModel = function(show)
+{
+	this.materials = [];
+	this.model = [];
+	this.Group = new THREE.Group();
+
+};
+
+LoadAModel.prototype = {
+	loadMaterial:function(url)
+	{
+
+	},
+
+	loadGeometry:function(url)
+	{
+
+	},
+
+	load_eye:function(url)
+	{
+
+	},
+
+	load_body:function()
+	{
+
+	}
+
+
+}
